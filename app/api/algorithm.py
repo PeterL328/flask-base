45c43871777274313ed06c9e3d084ee7cc0d31c7
ArchyYang <y464yang@uwaterloo.ca>
2020-04-11 20:08:03 -0400

    Add more api

--- app/api/algorithm.py
+++ app/api/algorithm.py
@@ -1,4 +1,61 @@
+# tuned thresholds for z_thresh
+THRESH_LOWER = 1.5
+THRESH_UPPER = 2.5
+THRESH_SEVERITY = 4
+DIFF = 1
+DIFF_SEVERITY = 0.5
+
 def calculate_potholes(z_accelerations):
     # returns a list of tuple containing long lat coordinates and severity level for potholes
     example_result = [(95, 40, 2), (54, 23, 1)]
     return example_result
+
+
+
+
+def z_thresh(sensor_readings):
+    lon = lat = 0
+    pothole = False
+    max_z = THRESH_UPPER
+    min_z = THRESH_LOWER
+    result = []
+    for val in sensor_readings:
+        z_acceleration = val["z_acc"]
+        long = val["long"]
+        lat = val["lat"]
+        print(long, lat)
+        if z_acceleration <= THRESH_LOWER or z_acceleration >= THRESH_UPPER:
+            pothole = True 
+            max_z = max(max_z, z_acceleration)
+            min_z = min(min_z, z_acceleration)
+            severity = 1 if max_z - min_z > THRESH_SEVERITY else 2
+            result.append((long, lat, severity))
+    return merge(result)
+
+def z_diff(sensor_readings):
+    lon = lat = 0
+    pothole = False
+    result = []
+    prev_data = None
+    for key, val in sensor_readings.items():
+        if prev_data != None:
+            z_acceleration = val["z_acc"]
+            current_diff = abs(z_acceleration - prev_data["z_acc"])
+            if current_diff >= DIFF:
+                pothole = True 
+                severity = 2 if current_diff * DIFF_SEVERITY > DIFF else 1
+                result.append((val["long"], val['lat'], severity))
+        prev_data = val
+    return merge(result)
+
+def merge(dataset):
+    if len(dataset) == 1:
+        return dataset
+    lon = dataset[0][0]
+    lat = dataset[0][1]
+    severity = dataset[0][2]
+    for i in range(1, len(dataset)):
+        lon = (dataset[i][0] + lon)/2
+        lat = (dataset[i][1] + lat)/2
+        severity = max(dataset[i][2], severity)
+    return [(lon, lat, severity)]
--- app/api/api.py
+++ app/api/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from .algorithm import calculate_potholes
+from .algorithm import z_thresh, z_diff
 from .. import easy_db
 
 api = Blueprint('api', __name__)
@@ -30,10 +31,8 @@ def store_data():
         lat = val['lat']
         z_acceleration = val["z_acc"]
         easy_db.execute(f"INSERT INTO raw_data VALUES (point({long}, {lat}), '{key}', {z_acceleration})")
-
     # Process window of acceleration values
     pothole_locations = calculate_potholes(z_acceleration)
-
     found_potholes = False if pothole_locations == [] else True
 
     if found_potholes:
@@ -58,3 +57,49 @@ def store_data():
 def fetch_potholes():
     potholes = easy_db.execute("SELECT location, severity, time FROM pothole")
     return jsonify({'success': True, 'result': [dict(row) for row in potholes]})
+
+@api.route('/ingest_all', methods=['POST'])
+def ingest_all():
+    json_data = request.get_json(force=True)
+    i = 0
+    while 1:
+        if str(i) in json_data:
+            val =json_data[str(i)]
+            time = val["time"]
+            long = val["long"]
+            lat = val['lat']
+            z_acceleration = val["z_acc"]
+            easy_db.execute(f"INSERT INTO raw_data VALUES ({i}, point({long}, {lat}), '{time}', {z_acceleration})")
+            i += 1
+        else:
+            break
+    result = {
+        'success': True,
+        'number_data': i,
+    }
+    return jsonify(result)
+
+@api.route('/compute_and_get_potholes', methods=['GET'])
+def update_potholes():
+    all_points = easy_db.execute("SELECT location[0] as long, location[1] as lat, time, z FROM raw_data order by id;")
+    params = []
+    temp = []
+    i = 0
+    for row in all_points:
+        if i == 50:
+            params.append(list(temp))
+            temp = []
+            i = 0
+        temp.append(dict(row))
+        i += 1
+    for i in params:
+        pothole_locations = z_thresh(i)
+        found_potholes = False if pothole_locations == [] else True
+        for long, lat, severity in pothole_locations:
+            easy_db.execute(f"INSERT INTO pothole VALUES (point({long}, {lat}), {severity})")
+
+    #z_thresh(dict(params))
+    result = {
+        'success': True,
+    }
+    return jsonify(result)
